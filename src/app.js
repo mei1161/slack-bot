ddce69d0ec8df68c780e390c6b9b99c95fc134c9
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-01-06 00:30:14 +0900

    create bot

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+import fs from 'fs-extra';
+import SlackBot from 'slackbots';
+
+// 設定の読み込み
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+const env = process.env;
+
+let bot = new SlackBot({
+  token: env.SLACK_ACCESS_TOKEN, 
+  name: env.SLACK_BOT_NAME
+});
+
+bot.on('start', function() {
+  console.log('start bot');
+});
+
+
+async function message_bot(data){
+  let channel = await search_channel_name(data.channel)
+  if(data.text === 'hello'){
+    bot.postMessageToChannel(channel, 'world!');
+  }
+}
+
+async function search_channel_name(id){
+  let channels = await bot.getChannels();
+  for(let channel of channels.channels){
+    if(channel.id == id){
+      return channel.name;
+    }
+  }
+}
+
+bot.on('message', async data => {
+  if(data.type == 'message'){
+    message_bot(data);
+  }
+
+});
